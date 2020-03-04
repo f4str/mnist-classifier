cb8f4eba4f2340f62b2da7f6f28a64bac895443a
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2020-03-04 01:45:38 -0500

    implemented early stopping

--- network1.py
+++ network1.py
@@ -11,8 +11,12 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 class NeuralNetwork:
 	def __init__(self):
+		self.sess = tf.Session()
+		
 		self.learning_rate = 0.5
 		self.batch_size = 128
+		
+		self.patience = 32
 		
 		self.num_input = 784
 		self.num_classes = 10
@@ -21,8 +25,13 @@ class NeuralNetwork:
 		self.build()
 	
 	def load_data(self):
-		self.data = input_data.read_data_sets('data/MNIST/', one_hot=True)
-	
+		mnist = input_data.read_data_sets('data/MNIST/', one_hot=True)
+		self.training_data = mnist.train
+		self.X_valid = mnist.validation.images
+		self.y_valid = mnist.validation.labels
+		self.X_test = mnist.test.images
+		self.y_test = mnist.test.labels
+		
 	def build(self):
 		self.x = tf.placeholder(tf.float32, [None, self.num_input])
 		self.y = tf.placeholder(tf.float32, [None, self.num_classes])
@@ -40,22 +49,48 @@ class NeuralNetwork:
 		self.prediction = tf.argmax(self.logits)
 	
 	def train(self, epochs=500):
-		with tf.Session() as session:
-			session.run(tf.global_variables_initializer())
+		self.sess.run(tf.global_variables_initializer())
+		total_train_loss = []
+		total_train_acc = []
+		total_valid_loss = [] 
+		total_valid_acc = []
+		best_acc = 0
+		no_acc_change = 0
+		
+		for e in range(epochs):
+			x_batch, y_batch = self.training_data.next_batch(self.batch_size)
+			feed_dict = {self.x: x_batch, self.y: y_batch}
+			self.sess.run(self.optimizer, feed_dict=feed_dict)
+			train_loss, train_acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
 			
-			for i in range(epochs):
-				x_batch, y_batch = self.data.train.next_batch(self.batch_size)
-				feed_dict = {self.x: x_batch, self.y: y_batch}
-				
-				session.run(self.optimizer, feed_dict=feed_dict)
-				
-				loss, acc = session.run([self.loss, self.accuracy], feed_dict=feed_dict)
-				print(f'epoch {i + 1}: loss = {loss:.4f}, training accuracy = {acc:.4f}')
-			print('training complete')
+			feed_dict = {self.x: self.X_valid, self.y: self.y_valid}
+			valid_loss, valid_acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
 			
-			feed_dict = {self.x: self.data.test.images, self.y: self.data.test.labels}
-			acc = session.run(self.accuracy, feed_dict=feed_dict)
-			print(f'test accuracy = {acc:.4f}')
+			total_train_loss.append(train_loss)
+			total_train_acc.append(train_acc)
+			total_valid_loss.append(valid_loss)
+			total_valid_acc.append(valid_acc)
+			
+			print(f'epoch {e + 1}: train loss = {train_loss:.4f}, train acc = {train_acc:.4f}, valid loss = {valid_loss:.4f}, valid acc = {valid_acc:.4f}')
+			
+			if valid_acc > best_acc:
+				best_acc = valid_acc
+				no_acc_change = 0
+			else:
+				no_acc_change += 1
+			
+			if no_acc_change >= self.patience:
+				print('early stopping')
+				break
+			
+		print('training complete')
+		
+		feed_dict = {self.x: self.X_test, self.y: self.y_test}
+		acc = self.sess.run(self.accuracy, feed_dict=feed_dict)
+		print(f'test accuracy = {acc:.4f}')
+		
+		return total_train_loss, total_train_acc, total_valid_loss, total_valid_acc
+
 
 if __name__ == '__main__':
 	net = NeuralNetwork()
